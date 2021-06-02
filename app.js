86b141da25b31e2973706830d16c507352144bc9
Ozoede Chukwuebuka Victor <chukwuebuka.victor.ozoede@gmail.com>
2021-06-02 17:06:58 +0100

    node event module

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-
-const { SomeMathObject } = require('./tutorial');
+//1.modules
+/*const { SomeMathObject } = require('./tutorial');
 const tutorial = require('./tutorial');
 
 
 console.log(tutorial.sum(15,11));
 console.log(tutorial.PI);
-console.log(new tutorial.SomeMathObject());
+console.log(new tutorial.SomeMathObject());*/
+
+
+//1. Event module
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('tutorial',() => {
+    console.log('Tutorial event has occured');
+});
+
+
+eventEmitter.emit('tutorial');
