0fdba0c055cbb22e28efb254835567a228424005
Ozoede Chukwuebuka Victor <chukwuebuka.victor.ozoede@gmail.com>
2021-06-02 16:48:14 +0100

    node modules

---
+++ app.js
@@ -0,0 +1,8 @@
+
+const { SomeMathObject } = require('./tutorial');
+const tutorial = require('./tutorial');
+
+
+console.log(tutorial.sum(15,11));
+console.log(tutorial.PI);
+console.log(new tutorial.SomeMathObject());
