3d81c6c70bf8a0221420c108bd05020bc6bc2d7f
Ozoede Chukwuebuka Victor <chukwuebuka.victor.ozoede@gmail.com>
2021-06-03 11:36:11 +0100

    deleted two files in a folder simultanously

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
-//File system 2(creating a folder a folder and a file inside the folder)
+// //File system 2(creating a folder a folder and a file inside the folder)
 fs = require('fs');
-fs.mkdir('my node js folder', (err)=>{
-    if(err)
-    console.log(err);
-    else
-    fs.writeFile('./my node js folder/nodejs.txt','I created this file inside my folder', (err)=>{
-        if (err)
-        console.log(err);
-        else
-        console.log('The file nodejs.txt successfully created')
-    });
+// fs.mkdir('my node js folder', (err)=>{
+//     if(err)
+//     console.log(err);
+//     else
+//     fs.writeFile('./my node js folder/nodejs.txt','I created this file inside my folder', (err)=>{
+//         if (err)
+//         console.log(err);
+//         else
+//         console.log('The file nodejs.txt successfully created')
+//     });
         
-});
+// });
 
-//Deleting a folder with a file inside it
-fs.unlink('./my node js folder/nodejs.txt',(err)=>{
-    if (err)
-    console.log(err);
-    else
-    fs.rmdir('my node js folder', (err)=>{
-        if (err)
-        console.log(err);
-        else
-        console.log('file and folder successfully deleted');
+// //Deleting a folder with a file inside it
+// fs.unlink('./my node js folder/nodejs.txt',(err)=>{
+//     if (err)
+//     console.log(err);
+//     else
+//     fs.rmdir('my node js folder', (err)=>{
+//         if (err)
+//         console.log(err);
+//         else
+//         console.log('file and folder successfully deleted');
+//     });
+// });
+
+
+//Delete multiple files in a folder simultanously
+fs.readdir('new_example',(err, files)=>{
+if(err)
+console.log(err);
+else
+for(let file of files){
+    fs.unlink('./new_example/' + file,(error)=>{
+if (err)
+console.log(err);
+else
+console.log('Successfully deleted files');
     });
+}
 });
