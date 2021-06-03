7ba51f30bdb51d91e76c917eab884750cbd22be2
Ozoede Chukwuebuka Victor <chukwuebuka.victor.ozoede@gmail.com>
2021-06-03 10:51:28 +0100

    created a folder and added a file inside the folder

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-//File system 2(creating a folder)
+//File system 2(creating a folder a folder and a file inside the folder)
 fs = require('fs');
 fs.mkdir('my node js folder', (err)=>{
     if(err)
     console.log(err);
     else
-    console.log('My node js folder successfully created');
+    fs.writeFile('./my node js folder/nodejs.txt','I created this file inside my folder', (err)=>{
+        if (err)
+        console.log(err);
+        else
+        console.log('The file nodejs.txt successfully created')
+    });
         
-})
-
-//Delete a folder
-fs.rmdir('my node js folder',(err)=>{
-    if (err)
-    console.log(err);
-    else
-    console.log('folder successfully deleted')
 });
+
