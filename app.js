1ac994819a66ab13994d8623a3bec8016b99e6c3
Ozoede Chukwuebuka Victor <chukwuebuka.victor.ozoede@gmail.com>
2021-06-03 13:28:32 +0100

    read and write streams

--- app.js
+++ app.js
@@ -1,43 +1,8 @@
-// //File system 2(creating a folder a folder and a file inside the folder)
-fs = require('fs');
-// fs.mkdir('my node js folder', (err)=>{
-//     if(err)
-//     console.log(err);
-//     else
-//     fs.writeFile('./my node js folder/nodejs.txt','I created this file inside my folder', (err)=>{
-//         if (err)
-//         console.log(err);
-//         else
-//         console.log('The file nodejs.txt successfully created')
-//     });
-        
-// });
-
-// //Deleting a folder with a file inside it
-// fs.unlink('./my node js folder/nodejs.txt',(err)=>{
-//     if (err)
-//     console.log(err);
-//     else
-//     fs.rmdir('my node js folder', (err)=>{
-//         if (err)
-//         console.log(err);
-//         else
-//         console.log('file and folder successfully deleted');
-//     });
-// });
-
-
-//Delete multiple files in a folder simultanously
-fs.readdir('new_example',(err, files)=>{
-if(err)
-console.log(err);
-else
-for(let file of files){
-    fs.unlink('./new_example/' + file,(error)=>{
-if (err)
-console.log(err);
-else
-console.log('Successfully deleted files');
-    });
-}
+//Readable and writable streams
+const fs = require('fs');
+const readStream = fs.createReadStream('./example.txt','utf8');
+const writeStream = fs.createWriteStream('./exapmle2.txt')
+readStream.on('data', (chunk)=>{
+    writeStream.write(chunk);
 });
+
