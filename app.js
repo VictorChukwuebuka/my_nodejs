0b4ab28286c39e41f6535c76d4c7a0d273932333
Ozoede Chukwuebuka Victor <chukwuebuka.victor.ozoede@gmail.com>
2021-06-03 20:40:37 +0100

    creating zip files and unziping the file

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-//pipes and pipe chaining(shorter way of creating a new file and 
+//pipe chaining:create zip file:(shorter way of creating a new file and 
 //transfering the content of the old file to new one)
 const fs = require('fs');
+const zlib = require('zlib');
+const gzip = zlib.createGzip();
 const readStream = fs.createReadStream('./example.txt','utf8');
-const writeStream = fs.createWriteStream('./exapmle2.txt')
-readStream.pipe(writeStream);
+const writeStream = fs.createWriteStream('./exapmle2.txt.gz')
+readStream.pipe(gzip).pipe(writeStream);
+
+
+//Create unzip
+const fs = require('fs');
+const zlib = require('zlib');
+const gunzip = zlib.createGunzip();
+const readStream = fs.createReadStream('./exapmle2.txt.gz');
+const writeStream = fs.createWriteStream('uncompressed.txt')
+readStream.pipe(gunzip).pipe(writeStream);
