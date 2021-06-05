e7e0ef6959603bd5b6b4b162848f22cf234435d6
Ozoede Chukwuebuka Victor <chukwuebuka.victor.ozoede@gmail.com>
2021-06-05 22:13:55 +0100

    serving http and file system module

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
-//Creatint http server using http module
-const http = require('http');
-const server = http.createServer((req,res)=>{
-    if (req.url === ('/')){
-        res.write('Hello world from nodejs');
-    res.end();
-    }
-    else{
-        res.write('Using some other domain');
-        res.end();
-    }
-    
-    
-});
+//serving static files with http and file system modules
 
-server.listen('3000');
+const http = require('http');
+const fs = require('fs');
+http.createServer((req,res)=>{
+    const readStream = fs.createReadStream('./static/index.html');
+    res.writeHead(200,{'Content-type':'text/html'});
+    readStream.pipe(res);
+}).listen(3000);
