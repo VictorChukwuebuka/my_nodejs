aa339bb53e344bbdd4843ea7b7a16f9cb3a1d6c8
Ozoede Chukwuebuka Victor <chukwuebuka.victor.ozoede@gmail.com>
2021-06-08 11:49:54 +0100

    get requests

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-//Express server
+//Express get request
 const express = require('express');
 const app = express();
 app.get('/',(req,res)=>{
 res.send('Hello World from nodejs');
 
+});
+app.get('/example',(req,res)=>{
+    res.send('hitting example route');
+});
+app.get('/example/:name/:age',(req,res)=>{
+    console.log(req.params)
+    console.log(req.query);
+    res.send(req.params.name + " : " + req.params.age);
 });
 
 app.listen(3000);
