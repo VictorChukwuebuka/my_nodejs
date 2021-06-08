2cb8cb269c311f8f840c21f60aef9d8fda459d39
Ozoede Chukwuebuka Victor <chukwuebuka.victor.ozoede@gmail.com>
2021-06-08 11:21:31 +0100

    my first express server

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-//serving static files with http and file system modules
+//Express server
+const express = require('express');
+const app = express();
+app.get('/',(req,res)=>{
+res.send('Hello World from nodejs');
 
-const http = require('http');
-const fs = require('fs');
-http.createServer((req,res)=>{
-    const readStream = fs.createReadStream('./static/index.html');
-    res.writeHead(200,{'Content-type':'text/html'});
-    readStream.pipe(res);
-}).listen(3000);
+});
+
+app.listen(3000);
