410ea294dc8ec49176eb969492c9c3f287ec3cf6
Ozoede Chukwuebuka Victor <chukwuebuka.victor.ozoede@gmail.com>
2021-06-08 17:28:52 +0100

    serving htmland css file

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-//Express get request
+//Serving static files using express eg htmls, css etc
 const express = require('express');
+const path = require('path');
 const app = express();
+app.use('/public',express.static(path.join(__dirname,'static')));
 app.get('/',(req,res)=>{
-res.send('Hello World from nodejs');
+res.sendFile(path.join(__dirname,'static','index.html'));
 
 });
 app.get('/example',(req,res)=>{
