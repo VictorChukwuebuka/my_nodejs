aa26a92aa49f052a4fa3ac903067426a9bd9a4e9
Ozoede Chukwuebuka Victor <chukwuebuka.victor.ozoede@gmail.com>
2021-06-09 15:34:46 +0100

    http post request using express and body parser module

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
-//Serving static files using express eg htmls, css etc
+//Http post request W/express and body parcer module
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
+
 app.use('/public',express.static(path.join(__dirname,'static')));
-app.get('/',(req,res)=>{
+app.use(bodyParser.urlencoded({extended: false}));
+
+ app.get('/',(req,res)=>{
 res.sendFile(path.join(__dirname,'static','index.html'));
 
 });
-app.get('/example',(req,res)=>{
-    res.send('hitting example route');
+
+app.post('/',(req,res)=>{
+    console.log(req.body);
+    res.send('successfully posted');
 });
-app.get('/example/:name/:age',(req,res)=>{
-    console.log(req.params)
-    console.log(req.query);
-    res.send(req.params.name + " : " + req.params.age);
-});
+
+// app.get('/example',(req,res)=>{
+//     res.send('hitting example route');
+// });
+
+
+// app.get('/example/:name/:age',(req,res)=>{
+//     console.log(req.params)
+//     console.log(req.query);
+//     res.send(req.params.name + " : " + req.params.age);
+// });
 
 app.listen(3000);
